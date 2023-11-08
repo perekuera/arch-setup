89bbeff85a9d3e11a58030fb7e8a76f6e00e7f3f
Pere Bestard <perekuera@gmail.com>
2023-11-08 19:00:28 +0100

    progress

--- setup.sh
+++ setup.sh
@@ -2,12 +2,20 @@
 
 #Install software
 sudo pacman -Syyu
-sudo pacman -S --needed base-devel pacman-contrib git vim
+sudo pacman -S --needed base-devel pacman-contrib bash-completion zsh zsh-completions x11-ssh-askpass
+sudo pacman -S --needed less git vim
+sudo pacman -S --needed alacritty vscode 
 
 #Yay install
 git clone https://aur.archlinux.org/yay.git ~/yay
 cd ~/yay
 makepkg -si
 
+yay -S google-chrome pfetch
+
+#Rate mirrors
 yay -S rate-mirrors
 sudo rate-mirrors --entry-country ES --disable-comments --save /etc/pacman.d/mirrorlist --allow-root arch --max-delay 500
+
+
+
