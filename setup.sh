bd18451d67eff9376ec73cbedff031bf798b3ff3
= <perekuera@gmail.com>
2023-11-08 11:52:50 +0100

    changes

--- setup.sh
+++ setup.sh
@@ -2,10 +2,12 @@
 echo "Actualizando pacman..."
 sudo pacman -Syyu
 
-sudo pacman -S less git reflector neofetch vscode chromium
+sudo pacman -S vim
 
 #sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 
 echo "Actualizando lista servidores..."
-sudo reflector --threads 4 --latest 20 --fastest 10 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
+#sudo reflector --threads 4 --latest 20 --fastest 10 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
+sudo pacman-mirrors -f 5 5
+
 #cat /etc/pacman.d/mirrorlist
