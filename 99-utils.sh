803a0a1ebb331774e54b71685aa1b74cd62d5f77
Pere Bestard <perekuera@gmail.com>
2024-02-02 11:07:15 +0100

    pacman problems fix

--- 99-utils.sh
+++ 99-utils.sh
@@ -1,2 +1,16 @@
 #Elimina packages no usados
 sudo pacman -Rns $(pacman -Qtdq)
+
+
+#PACMAN PROBLEMS
+
+sudo pacman -Syyu
+sudo pacman-key --verify
+sudo pacman-key --refresh-keys
+sudo pacman-key --populate archlinux
+sudo pacman -Sy archlinux-keyring
+sudo pacman -U archlinux-keyring
+
+
+sudo rm -R /var/lib/pacman/sync
+sudo -E pacman -Syu
