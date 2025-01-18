91f7e530484978f8d0b87854d113b41b15080958
Pere Bestard <p.bestard@globalia.com>
2025-01-18 08:25:05 +0100

    system backup

--- 01-bashrc-adds.sh
+++ 01-bashrc-adds.sh
@@ -2,11 +2,15 @@
 
 set -eu
 
+pfetch
+
+alias clear="clear && pfetch"
+
+OSH_THEME="agnoster"
+
 # default less options
 export LESS=-iXFR
-
-# or only for git
-#git config --global --replace-all core.pager "less -iXFR"
+# or only for Git: git config --global --replace-all core.pager "less -iXFR"
 
 #ssh-agent problem?
-export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh
+#export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/gcr/ssh
--- backup.sh
+++ backup.sh
@@ -2,6 +2,7 @@
 
 BACKUP_DIR="./system_backup"
 AWS_DIR="$HOME/.aws"
+SSH_DIR="$HOME/.ssh"
 
 # Si no existe la carpeta .aws en el directorio ./system_backup la crea
 if [ ! -d "$BACKUP_DIR/.aws" ]; then
@@ -9,6 +10,17 @@ if [ ! -d "$BACKUP_DIR/.aws" ]; then
 fi
 
 # Copiar los archivos config y credentials desde ~/.aws a ./system_backup/.aws
-cp -r "$AWS_DIR/config" "$AWS_DIR/credentials" "$BACKUP_DIR/.aws/"
+cp "$AWS_DIR/config" "$AWS_DIR/credentials" "$BACKUP_DIR/.aws/"
 
-echo "Archivos copiados exitosamente a $BACKUP_DIR/.aws"
+# .gitconfig
+cp ~/.gitconfig "$BACKUP_DIR"
+
+# .ssh
+if [ ! -d "$BACKUP_DIR/.ssh" ]; then
+    mkdir -p "$BACKUP_DIR/.ssh"
+fi
+
+cp "$SSH_DIR"/* "$BACKUP_DIR/.ssh/"
+
+
+echo "Backup realizado correctamente"
